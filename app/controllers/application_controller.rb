deda43e6953749761b4c8c8ca100eb5917c4a66a
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-10 15:08:28 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
+    redirect_to login_path unless current_user
   end
 
   def current_user
