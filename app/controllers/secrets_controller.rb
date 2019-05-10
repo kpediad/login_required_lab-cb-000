bb1a88faf03453ea26495ba0e0de49a72d85b214
Konstantinos Pediaditakis <pediadk@gmail.com>
2019-05-10 14:42:42 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,2 +1,12 @@
 class SecretsController < ApplicationController
+  before_action :require_login
+
+
+
+
+  private
+
+  def require_login
+    return head(:forbidden) unless session.include? :user_id
+  end
 end
